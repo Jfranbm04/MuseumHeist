52cd90ed11d2967cbe0627a31cd7062683a39fdf
Jfranbm04 <juanfranburgos2004@gmail.com>
2026-01-08 10:45:58 +0100

    CREATE: First Commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit = new RaycastHit();
+
+            if (Physics.Raycast(ray, out hit, 1000))
+            {
+                GetComponent<NavMeshAgent>().SetDestination(hit.point);
+            }
+        }
+
+      
+    }
+ }
